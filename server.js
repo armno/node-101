9c3d7f095a4424f39b7b196fe107406ad59648ca
Armno <monkeyarmno@gmail.com>
2015-02-26 13:27:13 +0700

    create a simple web server

---
+++ server.js
@@ -0,0 +1,10 @@
+var http = require('http');
+
+http.createServer(function(req, res) {
+	res.writeHead(200,  {
+		'Content-Type': 'text/plain'
+	});
+	res.end('Hello world!\n');
+}).listen(8888);
+
+console.log('server is running on port 8888');
